0f396a5d3c612b2fd316467d40115b1a3dd51e58
mako593 <orange8mako@gmail.com>
2026-09-16 14:53:40 +0900

    Update server.py

--- server.py
+++ server.py
@@ -63,8 +63,10 @@ def search_bakeries(
 
 
 if __name__ == "__main__":
+    import os
+
     mcp.run(
         transport="http",
         host="0.0.0.0",
-        port=8000
+        port=int(os.environ.get("PORT", 8000))
     )
